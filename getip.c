4659126dc8b653923a7424a08058f7187a462c2f
AnaGomes <ei10052@I00CN1102.salas.feupsig.fe.up.pt>
2012-11-12 16:26:44 +0000

    Adicionados ficheiros originais com comentarios extra.

---
+++ getip.c
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+
+int main(int argc, char *argv[])
+{
+	struct hostent *h;
+
+        if (argc != 2) {  
+            fprintf(stderr,"usage: getip address\n");
+            exit(1);
+        }
+        
+        
+/*
+struct hostent {
+	char    *h_name;	Official name of the host. 
+    	char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
+	int     h_addrtype;	The type of address being returned; usually AF_INET.
+    	int     h_length;	The length of the address in bytes.
+	char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
+				Host addresses are in Network Byte Order. 
+};
+
+#define h_addr h_addr_list[0]	The first address in h_addr_list. 
+*/
+
+/* Gethosbyname returns a struct of type hostent for the given host name.
+	Hostname is : - name of host
+				  - ipv4 address started by dot notation
+				  - ipv6 address in colon (or possibily dot) notation
+*/
+
+        if ((h=gethostbyname(argv[1])) == NULL) {  
+            herror("gethostbyname");
+            exit(1);
+        }
+
+        printf("Host name  : %s\n", h->h_name);
+		
+		/* inet_ntoa converts internet host address to IPV4
+		   and stors it in struct given*/
+        printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+
+        return 0;
+}
